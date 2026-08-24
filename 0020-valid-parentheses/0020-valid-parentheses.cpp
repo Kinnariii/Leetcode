6c18ca3a8a37efcb580a8258f799ecedfb34b383
Kinnari Dodiya <kinnaridodiya54@gmail.com>
2026-08-24 22:41:20 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.76%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
-        int n = s.length();
-        if(n==1) return false;
-        if(n==0) return true;
-        int i = 0;
-        while(i<n){
-            if(s[i] == '(' || s[i] == '{' || s[i]=='['){
-                st.push(s[i]);
-                i++;
+        stack<char> st;
+
+        for (char a : s) {
+            if (a == '(' || a == '{' || a == '[') {
+                st.push(a);
             }
-            else{
-                if(st.empty()) return false;
-                if(st.top() == '(' && s[i] == ')' || st.top() == '{' && s[i] == '}' || st.top() == '[' && s[i] == ']'){
-                    st.pop();
-                    i++;
+            else {
+                if (st.empty()) {
+                    return false;
                 }
-                else{
+
+                if ((a == ')' && st.top() == '(') ||
+                    (a == '}' && st.top() == '{') ||
+                    (a == ']' && st.top() == '[')) {
+                    st.pop();
+                }
+                else {
                     return false;
                 }
             }
         }
-       if(st.empty()) return true;
-       return false;
+
+        return st.empty();
     }
 };
